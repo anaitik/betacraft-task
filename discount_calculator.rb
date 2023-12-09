cd01ffcb8e5483e1a1c3750ada036f73e05dc6a8
nitik verma <61739812+anaitik@users.noreply.github.com>
2023-12-09 15:19:29 +0530

    class and basic structure

--- discount_calculator.rb
+++ discount_calculator.rb
@@ -1,1 +1,12 @@
+class DiscountCalculator
+  PRICING_TABLE = {
+    'Milk' => { unit_price: 3.97, sale_quantity: 2, sale_price: 5.00 },
+    'Bread' => { unit_price: 2.17, sale_quantity: 3, sale_price: 6.00 },
+    'Banana' => { unit_price: 0.99 },
+    'Apple' => { unit_price: 0.89 }
+  }
 
+  def initialize
+    @items = Hash.new(0)
+  end
+end
